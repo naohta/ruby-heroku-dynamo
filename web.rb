e5dcfb9c313ada431b51fd84270758bd1c2b6f34
Naohiro OHTA <naohta@gmail.com>
2012-12-27 01:19:27 +0900

    as_json3

--- web.rb
+++ web.rb
@@ -37,14 +37,6 @@ get '/products/as_html' do
   return s
 end
 
-
-def to_j(item) 
-
-  
-
-end
-
-
 get '/products/as_json' do
   content_type :json
   db = Dynamodb.db
@@ -75,6 +67,27 @@ get '/products/as_json2' do
     p "item.attributes.to_h", item.attributes.to_h
     p "JSON.generate(item.attributes.to_h)", JSON.generate(item.attributes.to_h)
     s += JSON.generate(item.attributes.to_h)
+  }
+  s += "]"
+  return s
+end
+
+
+get '/products/as_json3' do
+  content_type :json
+  db = Dynamodb.db
+  db.tables.each{|t| puts t.name}
+  tbl = db.tables['products']
+  tbl.load_schema
+  items = tbl.items()
+  s = "["
+  
+  first=true; items.each{ |item|
+    if(first) then first=false else s+="," end
+    p "item.attributes", item.attributes
+    p "item.attributes.to_h", item.attributes.to_h
+    p "JSON.generate(item.attributes.to_h)", JSON.generate(item.attributes.to_h)
+    s += JSON.pretty_generate(item.attributes.to_h)
   }
   s += "]"
   return s
